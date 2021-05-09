3fd66c31dbed022560c7f07b2db446909d068d26
Joan J <joanne.javillo@gmail.com>
2021-05-09 13:49:07 +0930

    added utils folder with api js

---
+++ my-app/src/utils/Api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+// Export an object containing methods we'll use for accessing the Random User API
+
+export default {
+  getRandomUser: function() {
+    return axios.get("https://randomuser.me/api/?results=5)");
+  },
+//   getDogsOfBreed: function(breed) {
+//     return axios.get("https://randomuser.me/)" + breed + "/images");
+//   },
+//   getBaseBreedsList: function() {
+//     return axios.get("https://dog.ceo/api/breeds/list");
+//   }
+};
